76a8faeeb4f17db18e47c73d19bdecbe7284661e
ipek <ipekcivicakan@gmail.com>
2021-05-25 16:33:28 -0400

    Initial project setup and nested serializer

---
+++ watchmate/watchlist_app/api/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from watchlist_app.models import WatchList, StreamPlatform
+
+
+class WatchListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WatchList
+        fields = "__all__"
+
+class StreamPlatformSerializer(serializers.ModelSerializer):
+    # NESTED SERIALIZER: 'watchlist' name come from model's "related_name". many= True because watchlist can have many movies
+    watchlist = WatchListSerializer(many = True, read_only = True)
+
+    class Meta:
+        model = StreamPlatform
+        fields = "__all__"
---
+++ watchmate/watchlist_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+# from watchlist_app.api.views import movie_list, movie_details
+from watchlist_app.api.views import WatchListAV, WatchListDetailAV, StreamPlatformAV
+
+urlpatterns = [
+    path('list/', WatchListAV.as_view(), name='movie-list'),
+    path('<int:pk>', WatchListDetailAV.as_view(), name='movie-detail'),
+    path('stream/', StreamPlatformAV.as_view(), name='stream')
+]
