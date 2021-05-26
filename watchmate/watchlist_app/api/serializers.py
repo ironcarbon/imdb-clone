77514ea27fc95d7c7159b2196b036f6758fc8cae
ipek <ipekcivicakan@gmail.com>
2021-05-25 21:04:12 -0400

    'HyperlinkedModelSerializer, Instead of using primary key, it uses url's

--- watchmate/watchlist_app/api/serializers.py
+++ watchmate/watchlist_app/api/serializers.py
@@ -7,10 +7,15 @@ class WatchListSerializer(serializers.ModelSerializer):
         model = WatchList
         fields = "__all__"
 
-class StreamPlatformSerializer(serializers.ModelSerializer):
+class StreamPlatformSerializer(serializers.HyperlinkedModelSerializer):
     # NESTED SERIALIZER: 'watchlist' name come from model's "related_name". many= True because watchlist can have many movies
+
     watchlist = WatchListSerializer(many = True, read_only = True)
+    # watchlist = serializers.StringRelatedField(many=True)
 
     class Meta:
         model = StreamPlatform
         fields = "__all__"
+
+
+#HyperlinkedModelSerializer helps us to access url for any particular element.
--- watchmate/watchlist_app/api/views.py
+++ watchmate/watchlist_app/api/views.py
@@ -13,7 +13,7 @@ from watchlist_app.api.serializers import StreamPlatformSerializer, WatchListSer
 class StreamPlatformAV(APIView):
     def get(self, request):
         platform = StreamPlatform.objects.all()
-        serializer = StreamPlatformSerializer(platform, many=True)
+        serializer = StreamPlatformSerializer(platform, many=True, context={'request': request})
         return Response(serializer.data)
 
     def post(self, request):
@@ -26,22 +26,12 @@ class StreamPlatformAV(APIView):
 
 
 class StreamPlatformDetailAV(APIView):
-    # def get(self, request, pk):
-    #     print('HERERERERE')
-    #     print('pk', pk)
-    #     try:
-    #         platform = StreamPlatform.objects.get(pk=pk)
-    #     except StreamPlatform.DoesNotExist:
-    #         return Response({'error': 'Not found'}, status=status.HTTP_404_NOT_FOUND)
-
-    #     serializer = StreamPlatformSerializer(platform)
-    #     return Response(serializer.data)
     def get(self, request, pk):
         try:
             platform = StreamPlatform.objects.get(pk=pk)
         except StreamPlatform.DoesNotExist:
             return Response({'error': 'Not found'}, status=status.HTTP_404_NOT_FOUND)
-        serializer = StreamPlatformSerializer(platform)
+        serializer = StreamPlatformSerializer(platform, context = {'request': request})
         return Response(serializer.data)
 
     def put(self, request, pk):
