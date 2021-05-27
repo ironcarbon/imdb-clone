d0054ae38d9c30253666d5f60d1470350fde7d12
ipek <ipekcivicakan@gmail.com>
2021-05-26 23:20:53 -0400

    overwrite queryset

--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 # from watchlist_app.api.views import movie_list, movie_details
-from watchlist_app.api.views import ReviewList, ReviewDetail, WatchListAV, WatchDetailAV, StreamPlatformAV, StreamPlatformDetailAV
+from watchlist_app.api.views import ReviewCreate, ReviewList, ReviewDetail, WatchListAV, WatchDetailAV, StreamPlatformAV, StreamPlatformDetailAV
 
 urlpatterns = [
     path('list/', WatchListAV.as_view(), name='movie-list'),
@@ -8,9 +8,10 @@ urlpatterns = [
     path('stream/', StreamPlatformAV.as_view(), name='stream-list'),
     path('stream/<int:pk>', StreamPlatformDetailAV.as_view(), name='stream-detail'),
 
-    path('review/', ReviewList.as_view(), name = 'review-list'),
-    path('review/<int:pk>', ReviewDetail.as_view(), name = 'review-detail'),
+    # path('review/', ReviewList.as_view(), name = 'review-list'),
+    # path('review/<int:pk>', ReviewDetail.as_view(), name = 'review-detail'),
 
-    # path('stream/<int:pk>/review', ReviewList.as_view(), name = 'review-list'),
-    # path('stream/review/<int:pk>', ReviewDetail.as_view(), name = 'review-detail')
+    path('stream/<int:pk>/review-create', ReviewCreate.as_view(), name = 'review-create'),
+    path('stream/<int:pk>/review', ReviewList.as_view(), name = 'review-list'),
+    path('stream/review/<int:pk>', ReviewDetail.as_view(), name = 'review-detail'),
 ]
