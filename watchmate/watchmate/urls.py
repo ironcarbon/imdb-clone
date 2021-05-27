6238a48167fbf708c6162a44fe2ecd6e8c91c15d
ipek <ipekcivicakan@gmail.com>
2021-05-27 14:58:34 -0400

    Temporary login and logout

--- watchmate/watchmate/urls.py
+++ watchmate/watchmate/urls.py
@@ -19,4 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('watch/', include('watchlist_app.api.urls')),
+    # temprorary login/logout page provider
+    path('api-auth', include('rest_framework.urls')),
 ]
